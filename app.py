5b54c39fe42df0900c913ab5d79d74f49c9c21c4
orel2287 <147095559+orel2287@users.noreply.github.com>
2024-12-10 19:49:33 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 import requests
+
 app = Flask(__name__)
 
 API_KEY = '47d89e1c6e7a4d908f47327c49d7bfeb'  # Замените на ваш API ключ OpenWeatherMap
 BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+CITY = 'Vladimir'  # Задайте город по умолчанию
 
-
-
-@app.route('/weather', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_weather():
-    city = request.args.get('city')  # Получаем название города из параметров запроса
-    if not city:
-        return jsonify({"error": "Please provide a city name."}), 400
-
     response = requests.get(BASE_URL, params={
-        'q': city,
+        'q': CITY,
         'appid': API_KEY,
         'units': 'metric'  # Для получения температуры в Цельсиях
     })
